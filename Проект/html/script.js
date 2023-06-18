82f0dcfa742a3ca97c56ff5564ccd2fc5db1c4b5
mykola159 <92953782+mykola159@users.noreply.github.com>
2023-06-18 14:37:56 +0300

    Add files via upload

---
+++ Проект/html/script.js
@@ -0,0 +1,19 @@
+//const selectEl = document.getElementById('select');
+//const registrationImage = document.getElementById('planet-img');
+
+//selectEl.addEventListener('change', function () {
+
+   // switch (true) {
+      //  case this.value === 'audi R8': registrationImage.src = "C:\Users\Mykola\Desktop\Проект\img\audi R8.jpg";
+      //      break;
+     //   case this.value === 'audi A6': registrationImage.src = "C:\Users\Mykola\Desktop\Проект\img\Audi A6.jpg";
+    //        break;
+   // }
+
+//})
+//button.addEventListener('click', function () {
+ //   console.log(this.className);
+ //   this.classList.toggle('form-submit-button');
+//})
+
+
